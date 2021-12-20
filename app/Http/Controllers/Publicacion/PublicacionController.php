e98ed7f5926b3bc37d440f35a68376681c91d4b7
Macarena Ramos <macarenaarp@gmail.com>
2021-12-20 14:43:10 -0300

    Editar funcionanado

--- app/Http/Controllers/Publicacion/PublicacionController.php
+++ app/Http/Controllers/Publicacion/PublicacionController.php
@@ -120,9 +120,50 @@ class PublicacionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ValidacionesPublicacion $request, $id)
     {
-        //
+        $publicacion = Publicacion::find($id);
+        $publicacion->usuario_id = Auth::user()->usuario_id;
+        $publicacion->titulo = $request->titulo;
+        $publicacion->nombre_semilla = $request->nombre_semilla;
+        $publicacion->tipo_semilla_id = $request->tipo_semilla_id;
+        $publicacion->descripcion = $request->descripcion;
+        $publicacion->porcentaje_THC = str_replace(',', '.', $request->porcentaje_THC);
+        $publicacion->porcentaje_CBD = str_replace(',', '.', $request->porcentaje_CBD);
+        $publicacion->porcentaje_indica = str_replace(',', '.', $request->porcentaje_indica);
+        $publicacion->porcentaje_sativa = str_replace(',', '.', $request->porcentaje_sativa);
+        $publicacion->porcentaje_ruderalis = str_replace(',', '.', $request->porcentaje_ruderalis);
+        $publicacion->tiempo_floracion = str_replace('$ ', '', (str_replace('.', '', $request->tiempo_floracion)));
+        $publicacion->produccion_interior = str_replace('$ ', '', (str_replace('.', '', $request->produccion_interior)));
+        $publicacion->produccion_exterior = str_replace('$ ', '', (str_replace('.', '', $request->produccion_exterior)));
+        $publicacion->altura = str_replace('$ ', '', (str_replace('.', '', $request->altura)));
+        $publicacion->semillas_paquete = str_replace('$ ', '', (str_replace('.', '', $request->semillas_paquete)));
+        $publicacion->precio = str_replace('$ ', '', (str_replace('.', '', $request->precio)));
+        $publicacion->stock = str_replace('$ ', '', (str_replace('.', '', $request->stock)));
+        $publicacion->fecha_actualizacion = new DateTime();
+        $publicacion->activa = true;
+        
+        PublicacionFoto::where('publicacion_id', $id)->delete();
+        if ($publicacion->save()) {
+            if (count((array) $request->fotos) > 0) {
+                foreach ((array) $request->fotos as $key => $f) {
+                    if (isset($f)) {
+                        $size = Storage::size('public/' . $f);
+                        $foto = new PublicacionFoto();
+                        $foto->publicacion_id = $publicacion->publicacion_id;
+                        $foto->foto = $f;
+                        $foto->size = $size;
+                        $foto->save();
+                    }
+                }
+            }
+        }
+        $notificacion = array(
+            'mensaje' => 'Publicación actualizada con éxito',
+            'tipo' => 'success',
+            'titulo' => 'Publicaciones'
+        );
+        return redirect('/publicaciones')->with($notificacion);
     }
 
     /**
--- resources/views/includes/boton-form-editar.blade.php
+++ resources/views/includes/boton-form-editar.blade.php
@@ -1,2 +1,2 @@
 <a href="{{URL::previous()}}" type="reset" class="btn btn-secondary mr-2">Cancelar</a>
-<button type="submit" class="btn btn-consex text-white" id="actualizar">Actualizar</button>
+<button type="submit" class="btn btn-primary" id="actualizar">Actualizar</button>
